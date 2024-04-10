8e3db93da3d1e9beff9daef85b338feea2215e88
pukann <113951267+siksakas@users.noreply.github.com>
2024-04-10 11:58:55 -0700

    Update spotipi.py

--- spotipi.py
+++ spotipi.py
@@ -2,23 +2,19 @@ import requests
 import base64
 import random
 
-# to access API
 client_id = '65a6e47f54f7483a9b40bf72d796bbb8'
 client_secret = 'f1edc678de234c2a9c6d2c10bb1c9f00'
 
-# gets the access token
 def get_access_token(client_id, client_secret):
 
     client_creds = client_id + ":" + client_secret
     client_creds_b64 = base64.b64encode(client_creds.encode()) 
-    #basically we need to encode the credentials in order for it to work with spotify API since the API expects this kind of format for client credentials
-
+    
     token_data = {
         "grant_type": "client_credentials"
     }
     token_headers = {
         "Authorization": "Basic {}".format(client_creds_b64.decode())
-        # .format puts decoded client_creds_b64 into the {}
     }
 
     r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
@@ -26,8 +22,7 @@ def get_access_token(client_id, client_secret):
         return r.json()['access_token']
     return None
 
-# gets random song from playlist
-def get_random_song(access_token, playlist_id, choice):
+def get_random_song(playlist_id, choice):
     headers = {
         'Authorization': 'Bearer {}'.format(access_token)
     }
@@ -46,15 +41,13 @@ def get_random_song(access_token, playlist_id, choice):
 
 access_token = get_access_token(client_id, client_secret)
 
-if access_token: #checks to see if we have access token
+if access_token:
     while True:
         choice = input("Enter 'song' to get a random song or 'artist' to get a random artist or anything else to quit: ").lower()
         if choice in ['song','artist']:
-            for i in range(5):
-        # playlist ID from spotify
-                playlist_id = '37i9dQZF1DXcBWIGoYBM5M'
-        # Get a random song
-                item_name, artist_name = get_random_song(access_token, playlist_id, choice)
+            playlist_id = '37i9dQZF1DXcBWIGoYBM5M'
+            for i in range(5):   
+                item_name, artist_name = get_random_song(playlist_id, choice)
                 if item_name and artist_name:
                     print("Random Song: {} by {}".format(item_name, artist_name))
                 elif artist_name:
